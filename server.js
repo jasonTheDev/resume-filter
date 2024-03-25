cdd8d32bb68b792307f5758fc4278cb2f74fb2df
Jason Kepler <jasonkepler1@gmail.com>
2024-03-25 23:19:22 +0000

    Serve client app from express endpoint

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send("Resume Builder!");
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/api', (req, res) => {
