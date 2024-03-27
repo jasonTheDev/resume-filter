4a71446813cd2e6d400c63b4215f63bc349aefa4
Jason Kepler <jasonkepler1@gmail.com>
2024-03-27 14:47:47 +0000

    remove edit button

--- client/src/components/Experience.js
+++ client/src/components/Experience.js
@@ -1,15 +1,10 @@
-import Stack from 'react-bootstrap/Stack';
-import Button from 'react-bootstrap/Button';
 import BulletPoint from './BulletPoint';
 import '../styles/common.css'
 
 function Experience({ position, bullets }) {
   return (
     <div class="mb-4 mt-4">
-      <Stack direction="horizontal">
-        <h2>{position}</h2>
-        <Button className="m-2 ms-auto" variant="outline-secondary">Edit</Button>
-      </Stack>
+      <h2>{position}</h2>
       <ul>
         {
           bullets.map((bullet, i) =>
